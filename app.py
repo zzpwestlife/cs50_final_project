a56f82e057731f091f784205afdbcc1e82d2c198
joeyzou <joeyzou@futunn.com>
2023-11-01 11:59:11 +0800

    register and login

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_session import Session
+from helpers import login_required
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:secret@127.0.0.1:33072/flask_db'
 app.config['SECRET_KEY'] = '2b1a07d8b1f3b2c8de3f6b2d5d7f8a7b'
+app.secret_key = '2b1a07d8b1f3b2c8de3f6b2d5d7f8a7b'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 from models import User, Todo
 
 
 @app.route('/')
 @app.route('/todo_list')
+@login_required
 def todo_list():
+    user_id = session["user_id"]
+    if user_id is None:
+        return redirect("/login")
+
     todos = Todo.query.all()
     return render_template('todo_list.html', todos=todos)
 
@@ -52,6 +64,43 @@ def delete_todo(todo_id):
         flash('To-Do item deleted successfully!', 'success')
         return redirect(url_for('todo_list'))
     return render_template('delete_todo.html', todo=todo)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        existing_user = User.query.filter(
+            db.or_(User.username == username, User.email == email)
+        ).first()
+        if existing_user:
+            flash('Username or email already exists. Please try a different one.')
+            return redirect(url_for('register'))
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful! You can now log in.')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    session.clear()
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email, password=password).first()
+        if user and user.verify_password(password):
+            session["user_id"] = user["id"]
+            return redirect('/todo_list')
+        else:
+            flash('Invalid email or password', 'error')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,13 +1,30 @@
 from app import db
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return '<User %r>' % self.username
 
 
 class Todo(db.Model):
