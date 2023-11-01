e06bc911a324e4182b4d3454689af5514e453a53
joeyzou <joeyzou@futunn.com>
2023-11-01 11:35:46 +0800

    todo curd

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:secret@127.0.0.1:33072/flask_db'
+app.config['SECRET_KEY'] = '2b1a07d8b1f3b2c8de3f6b2d5d7f8a7b'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -12,9 +12,11 @@ from models import User, Todo
 
 
 @app.route('/')
-def index():
+@app.route('/todo_list')
+def todo_list():
     todos = Todo.query.all()
-    return render_template('index.html', todos=todos)
+    return render_template('todo_list.html', todos=todos)
+
 
 @app.route('/add_todo', methods=['GET', 'POST'])
 def add_todo():
@@ -24,9 +26,33 @@ def add_todo():
         todo = Todo(title=title, description=description)
         db.session.add(todo)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('todo_list'))
     return render_template('add_todo.html')
 
-#
+
+# Method for editing a to-do item
+@app.route('/edit_todo/<int:todo_id>', methods=['GET', 'POST'])
+def edit_todo(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        db.session.commit()
+        flash('To-Do item updated successfully!', 'success')
+        return redirect(url_for('todo_list'))
+    return render_template('edit_todo.html', todo=todo)
+
+
+# Method for deleting a to-do item
+@app.route('/delete_todo/<int:todo_id>', methods=['GET', 'POST'])
+def delete_todo(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    if request.method == 'POST':
+        db.session.delete(todo)
+        db.session.commit()
+        flash('To-Do item deleted successfully!', 'success')
+        return redirect(url_for('todo_list'))
+    return render_template('delete_todo.html', todo=todo)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
